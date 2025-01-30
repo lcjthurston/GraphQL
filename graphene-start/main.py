180ea19d2c0f22f0312191fa0ad1200737c85eb8
Luke Thurston <90153234+lcjthurston@users.noreply.github.com>
2025-01-30 10:18:12 -0600

    added dummy data store and root value

--- graphene-start/main.py
+++ graphene-start/main.py
@@ -1,19 +1,46 @@
-from graphene import Schema, ObjectType, String
+from graphene import Schema, ObjectType, String, Int, Field
+
+
+class UserType(ObjectType):
+    id = Int()
+    name = String()
+    age = Int()
+
 
 class Query(ObjectType):
-    hello = String(name=String(default_value="world"))
+    user = Field(UserType, user_id=Int())
 
-    def resolve_hello(self, info, name):
-        return f"Hello {name}"
+    # dummy data store
+    users = [
+        {"id": 1, "name": "Andy Doe", "age": 33},
+        {"id": 2, "name": "Andia Doe", "age": 34},
+        {"id": 3, "name": "Julie Sullivan", "age": 31},
+        {"id": 4, "name": "John Barber", "age": 29}
+    ]
+
+    @staticmethod
+    def resolve_user(root, info, user_id):
+        matched_users = [user for user in Query.users if user["id"] == user_id]
+        return matched_users[0] if matched_users else None
+    
+
+# OOP aside
+# - instance methods (first argument is self)
+# - class methods (first argument is cls)
+# - static methods (root, root_value, or source)
 
 schema = Schema(query=Query)
 
 gql = '''
-{
-    hello(name: "graphql")
+query {
+    user(userId: 4) {
+        id
+        name
+        age    
+    }
 }
 '''
 
 if __name__ == "__main__":
-    result = schema.execute(gql)
+    result = schema.execute(gql, root_value="some other data source")
     print(result)
