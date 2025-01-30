14049f34e50dd6e9728eae05e901d829f0ae4a20
Luke Thurston <90153234+lcjthurston@users.noreply.github.com>
2025-01-30 14:40:28 -0600

    GQL with Mutations

--- graphene-start/main.py
+++ graphene-start/main.py
@@ -8,7 +8,7 @@ class UserType(ObjectType):
 
 
 class CreateUser(Mutation):
-    class Arugments:
+    class Arguments:
         name = String()
         age = Int()
 
@@ -18,6 +18,7 @@ class CreateUser(Mutation):
         user = {"id": len(Query.users) + 1, "name": name, "age": age}
         Query.users.append(user)
         return CreateUser(user=user)
+
 
 class Query(ObjectType):
     user = Field(UserType, user_id=Int())
@@ -40,6 +41,7 @@ class Query(ObjectType):
     def resolve_users_by_min_age(root, info, min_age):
         return [user for user in Query.users if user["age"] >= min_age]
 
+
 class Mutation(ObjectType):
     create_user = CreateUser.Field()
 
@@ -50,9 +52,18 @@ class Mutation(ObjectType):
 
 schema = Schema(query=Query, mutation=Mutation)
 
+gql_2 = '''
+query {
+    user(userId: 5) {
+        name
+        age    
+    }
+}
+'''
+
 # gql = '''
 # query {
-#     user(userId: 4) {
+#     usersByMinAge(minAge: 30) {
 #         id
 #         name
 #         age    
@@ -61,15 +72,19 @@ schema = Schema(query=Query, mutation=Mutation)
 # '''
 
 gql = '''
-query {
-    usersByMinAge(minAge: 30) {
-        id
-        name
-        age    
+mutation {
+    createUser(name: "New User", age: 25) {
+        user {
+            id
+            name
+            age
+        }    
     }
 }
 '''
 
 if __name__ == "__main__":
-    result = schema.execute(gql, root_value="some other data source")
+    result = schema.execute(gql)
+    print(result)
+    result = schema.execute(gql_2)
     print(result)
