e9e2e79a40b16224311f9ef8b70327f8c53ce0b6
Luke Thurston <90153234+lcjthurston@users.noreply.github.com>
2025-01-30 14:15:51 -0600

    List Type Field and Resolver

--- graphene-start/main.py
+++ graphene-start/main.py
@@ -1,4 +1,4 @@
-from graphene import Schema, ObjectType, String, Int, Field
+from graphene import Schema, ObjectType, String, Int, Field, List, Mutation
 
 
 class UserType(ObjectType):
@@ -7,8 +7,21 @@ class UserType(ObjectType):
     age = Int()
 
 
+class CreateUser(Mutation):
+    class Arugments:
+        name = String()
+        age = Int()
+
+    user = Field(UserType)
+
+    def mutate(self, info, name, age):
+        user = {"id": len(Query.users) + 1, "name": name, "age": age}
+        Query.users.append(user)
+        return CreateUser(user=user)
+
 class Query(ObjectType):
     user = Field(UserType, user_id=Int())
+    users_by_min_age = List(UserType, min_age=Int())
 
     # dummy data store
     users = [
@@ -23,17 +36,33 @@ class Query(ObjectType):
         matched_users = [user for user in Query.users if user["id"] == user_id]
         return matched_users[0] if matched_users else None
     
+    @staticmethod
+    def resolve_users_by_min_age(root, info, min_age):
+        return [user for user in Query.users if user["age"] >= min_age]
+
+class Mutation(ObjectType):
+    create_user = CreateUser.Field()
 
 # OOP aside
 # - instance methods (first argument is self)
 # - class methods (first argument is cls)
 # - static methods (root, root_value, or source)
 
-schema = Schema(query=Query)
+schema = Schema(query=Query, mutation=Mutation)
+
+# gql = '''
+# query {
+#     user(userId: 4) {
+#         id
+#         name
+#         age    
+#     }
+# }
+# '''
 
 gql = '''
 query {
-    user(userId: 4) {
+    usersByMinAge(minAge: 30) {
         id
         name
         age    
